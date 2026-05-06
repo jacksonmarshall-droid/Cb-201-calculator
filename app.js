32884e45542d2603ff6920f76fe8af70e2f2c57c
jacksonmarshall-droid <jackson.marshall@codedbykids.org>
2026-05-06 22:48:39 +0000

    added stuff more stuff

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 function  add() {
-    const firstval=  document.getElementById("num1").value
+    const firstval=  Number(document.getElementById("num1").value)
     console.log(firstval)
+
+    const secondval= Number(document.getElementById("num2").value)
+    console.log(secondval)
+
+    // variable that is sum of numbers
+    const answer=(firstval + secondval)
+    console.log(answer)
+    document.getElementById("answer").textContent=answer
+}
+function subtract(){
+    const firstval=  Number(document.getElementById("num1").value)
+    console.log(firstval)
+
+    const secondval= Number(document.getElementById("num2").value)
+    console.log(secondval)
+
+    const difference=(firstval-secondval)
+    console.log(difference)
+    document.getElementById("difference").textContent=difference
 }
